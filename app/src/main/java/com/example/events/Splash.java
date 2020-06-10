e7d5eb4c9cf415477a28cc92f3296b2207f50eac
gfbritto <gfbritto@outlook.com>
2020-06-10 00:38:39 -0300

    Splash funcional sem bug
    Signed-off-by: gfbritto <gfbritto@outlook.com>

---
+++ app/src/main/java/com/example/events/Splash.java
@@ -0,0 +1,28 @@
+package com.example.events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
