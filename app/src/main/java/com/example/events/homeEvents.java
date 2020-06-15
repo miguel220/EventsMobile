5448e00b1dd5708faa228a3e47eb092dd6d75332
gfbritto <gfbritto@outlook.com>
2020-06-14 23:39:45 -0300

    drawer menu ok
    Signed-off-by: gfbritto <gfbritto@outlook.com>

--- app/src/main/java/com/example/events/homeEvents.java
+++ app/src/main/java/com/example/events/homeEvents.java
@@ -1,18 +1,37 @@
 package com.example.events;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class homeEvents extends AppCompatActivity {
+
+    //variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_events);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        //hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //toolbar
         setSupportActionBar(toolbar);
+
+        //navigation drawer menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
