cfc87f377ed8bb22cdbc9857f03677def47c42d9
gfbritto <gfbritto@outlook.com>
2020-06-20 23:31:05 -0300

    drawer menu faltando apenas os intents
    Signed-off-by: gfbritto <gfbritto@outlook.com>

--- app/src/main/java/com/example/events/homeEvents.java
+++ app/src/main/java/com/example/events/homeEvents.java
@@ -1,15 +1,21 @@
 package com.example.events;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class homeEvents extends AppCompatActivity {
+public class homeEvents extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //variables
     DrawerLayout drawerLayout;
@@ -30,8 +36,37 @@ public class homeEvents extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //navigation drawer menu
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+
+    @ Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_event:
+                break;
+            case R.id.nav_config:
+                Intent intent = new Intent(homeEvents.this,Config.class);
+                startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
