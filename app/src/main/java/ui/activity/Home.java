879e7cb929fcdb6bb24f6b7ff783e1721d11f314
Miguel Batista <56230892+miguel220@users.noreply.github.com>
2020-09-13 14:48:16 -0300

    Navegações
    navegações okay, sem erros

--- app/src/main/java/ui/activity/Home.java
+++ app/src/main/java/ui/activity/Home.java
@@ -1,57 +1,71 @@
 package ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 
 import com.example.events.R;
 
 public class Home extends AppCompatActivity {
 
-    ImageButton btnIrParaEventos, btnIrParaQrCode, btnIrParaPreferencias, btnAbrirLogout;
+    GridLayout myGrid;
+    Animation frombottom;
+    CardView cardEnventos, cardPreferencias, cardQrCode, cardSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnIrParaEventos = findViewById(R.id.btnIrParaEventos);
-        btnIrParaQrCode = findViewById(R.id.btnIrParaQrCode);
-        btnIrParaPreferencias = findViewById(R.id.btnIrParaPreferencias);
-        btnAbrirLogout = findViewById(R.id.btnAbrirLogout);
+        myGrid = (GridLayout) findViewById(R.id.myGrid);
 
-        // Criar a classe e activity da lista de eventos
-        //btnIrParaEventos.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        Intent intent = new Intent(getApplicationContext(), listaEventos.class);
-        //        startActivity(intent);
-        //    }
-        //});
+        frombottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.frombottom);
 
-        btnIrParaQrCode.setOnClickListener(new View.OnClickListener() {
+        cardsButtons();
+
+        cardSair();
+
+        cardQrCode();
+
+        cardeDePreferencias();
+    }
+
+    private void cardsButtons() {
+        cardPreferencias = findViewById(R.id.cardPreferencias);
+        cardQrCode = findViewById(R.id.cardQrCode);
+        cardSair = findViewById(R.id.cardSair);
+    }
+
+    private void cardSair() {
+        cardSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), QrCodeActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
+    }
 
-        btnIrParaPreferencias.setOnClickListener(new View.OnClickListener() {
+    private void cardQrCode() {
+        cardQrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), QrCodeActivity.class));
+            }
+        });
+    }
+
+    private void cardeDePreferencias() {
+        cardPreferencias.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Settings.class);
-                startActivity(intent);
-            }
-        });
-
-        btnAbrirLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LogoutPopup.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/ui/activity/Settings.java
+++ app/src/main/java/ui/activity/Settings.java
@@ -3,14 +3,28 @@ package ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.events.R;
 
 public class Settings extends AppCompatActivity {
 
+    Button btnVoltarHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        btnVoltarHome = findViewById(R.id.btnVoltarHome);
+
+        btnVoltarHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
